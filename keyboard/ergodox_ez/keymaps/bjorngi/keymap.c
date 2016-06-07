e85094e041607bdeb5fe286204c237b36183c724
Bjørn Gilstad <bjorn@skylabs.no>
2016-06-07 09:43:40 +0200

    Change L1

--- keyboard/ergodox_ez/keymaps/bjorngi/keymap.c
+++ keyboard/ergodox_ez/keymaps/bjorngi/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| [ {  |           | ] }  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  | , <  |  . > | /  ? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |   <  |   >  |      |      | `  ~ |                                       | VolUp |VolDwn| Mute |      | L1  |
+ *   |   <  |   >  |      |  `  ~ | L2  |                                       | ~L1  |VolUp |VolDwn| Mute |  L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | PgUp |PgDwn |       | Home |  End   |
@@ -33,20 +33,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,              KC_1,         KC_2,         KC_3,      KC_4,        KC_5,     KC_LPRN,
-        KC_TAB,              KC_Q,         KC_W,         KC_E,      KC_R,        KC_T,     LT(SYMB, KC_SCLN),
+        KC_TAB,              KC_Q,         KC_W,         KC_E,      KC_R,        KC_T,     KC_SCLN, 
         KC_ESC,              KC_A,         KC_S,         KC_D,      CTL_T(KC_F), KC_G,
         KC_LSFT,             KC_Z,         KC_X,         KC_C,      KC_V,        KC_B,     KC_LBRC,
-          KC_TRNS,             KC_TRNS,      KC_TRNS,      KC_TRNS,   KC_GRV,
+        KC_TRNS,             KC_TRNS,      KC_TRNS,      KC_GRV,    MO(SYMB),
 
                                                                                 KC_PGUP, KC_PGDOWN,
                                                                                            KC_HOME,
                                                                        KC_ENT,  KC_BSPC,   KC_LALT,
         // right hand
              KC_RPRN,           KC_6,       KC_7,      KC_8,      KC_9,     KC_0,               KC_MINS,
-             MT(SYMB, KC_COLN), KC_Y,       KC_U,      KC_I,      KC_O,     KC_P,               KC_BSLS,
+             KC_COLN,           KC_Y,       KC_U,      KC_I,      KC_O,     KC_P,               KC_BSLS,
                                 KC_H,ALT_T(KC_J),      KC_K,      KC_L,     LT(MDIA, KC_SCLN),  KC_QUOT,
              KC_RBRC,           KC_N,       KC_M,      KC_COMM,   KC_DOT,   KC_SLSH,            KC_RSFT,
-                                         KC_VOLU,      KC_VOLD,   KC_MUTE,  KC_TRNS,            KC_FN1,
+                                            MO(SYMB),  KC_VOLU,   KC_VOLD,  KC_MUTE,            KC_FN1,
 
              KC_HOME, KC_END,
              KC_TRNS,
